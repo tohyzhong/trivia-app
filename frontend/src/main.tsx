be79eb523e508e519decc69a71217e67f6186bd2
tohyzhong <tohyzhong@gmail.com>
2025-05-24 21:13:57 +0800

    feat: Register ag-grid community modules in main application file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+import React from 'react';
 
+ModuleRegistry.registerModules([AllCommunityModule]);
 const root = document.getElementById('root') as HTMLElement
 if (root !== null) {
   createRoot(root).render(
