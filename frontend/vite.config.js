76da8d8e7b36327ef3b394c175e97354adb6855d
tohyzhong <tohyzhong@gmail.com>
2025-05-27 14:12:10 +0800

    fix: Update API proxy URL to use environment variable for better configuration management

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': process.env.VITE_API_URL
     }
   },
   assetsInclude: ['**/*.jpg', '**/*.png'],
