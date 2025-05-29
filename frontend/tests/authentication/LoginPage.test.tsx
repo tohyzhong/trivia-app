0336feca16e64fed13e2dbb5fffc229ff55d6679
Blerargh <49093336+Blerargh@users.noreply.github.com>
2025-05-29 23:29:00 +0800

    Add testing setup and basic initial tests for LoginPage component

---
+++ frontend/tests/authentication/LoginPage.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { it, expect, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux'
+import store from '../../src/redux/store'
+import { MemoryRouter } from 'react-router-dom';
+import LoginPage from '../../src/components/authentication/subcomponents/LoginPage'
+
+describe('Authentication', () => {
+  it('should render login page', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginPage />
+        </MemoryRouter>
+      </Provider>
+    )
+
+    expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+    expect(screen.getByText('Forgot Password?')).toBeInTheDocument();
+    expect(screen.getByText('Don\'t have an account?')).toBeInTheDocument();
+    expect(screen.getByText('Sign up here!')).toBeInTheDocument();
+  })
+})
