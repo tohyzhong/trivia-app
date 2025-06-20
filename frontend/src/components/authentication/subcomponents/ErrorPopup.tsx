b88dc0ea45ca38f1df286f1798c3e2f163064429
tohyzhong <tohyzhong@gmail.com>
2025-06-20 15:33:48 +0800

    feat: enhance ErrorPopup component with success state styling

--- frontend/src/components/authentication/subcomponents/ErrorPopup.tsx
+++ frontend/src/components/authentication/subcomponents/ErrorPopup.tsx
@@ -7,29 +7,34 @@ import "../../../styles/errorpopup.css";
 interface Props {
   message: string;
   setMessage: (message: string) => void; // Optional setter for message
+  success?: boolean;
 }
 
-const ErrorPopup: React.FC<Props> = (props) => {
-  if (!props.message) {
+const ErrorPopup: React.FC<Props> = ({
+  message,
+  setMessage,
+  success = false
+}) => {
+  if (!message) {
     return null;
   } else
     return (
       <motion.div
-        className="invalid-token"
+        className={`invalid-token ${success ? "success" : ""}`}
         initial={{ opacity: 0, y: "-20%", x: "-50%" }}
         animate={{
           opacity: 1,
           y: "0%",
           x: "-50%",
-          transition: { duration: 0.3 },
+          transition: { duration: 0.3 }
         }}
       >
         <IoIosInformationCircle className="information-icon" />
-        <p>{props.message}</p>
+        <p>{message}</p>
         <RxCross2
           type="button"
           className="close-icon"
-          onClick={() => props.setMessage("")}
+          onClick={() => setMessage("")}
         />
       </motion.div>
     );
