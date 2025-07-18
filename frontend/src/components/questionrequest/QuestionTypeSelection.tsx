529409ae05536e4c86aeec5d6d7c936185012e80
tohyzhong <tohyzhong@gmail.com>
2025-07-18 22:45:04 +0800

    chore: styling fixes for buttons in question-request panel

--- frontend/src/components/questionrequest/QuestionTypeSelection.tsx
+++ frontend/src/components/questionrequest/QuestionTypeSelection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../../redux/store";
+import "../../styles/questionsubmissionform.css";
 
 const QuestionTypeSelection: React.FC = () => {
   const navigate = useNavigate();
