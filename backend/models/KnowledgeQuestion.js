590612c4225a8176907f923aa8ce11faa55b1db7
Blerargh <49093336+Blerargh@users.noreply.github.com>
2025-07-18 20:21:31 +0800

    feat: update KnowledgeQuestion schema

--- backend/models/KnowledgeQuestion.js
+++ backend/models/KnowledgeQuestion.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const knowledgeQuestionSchema = new mongoose.Schema({
   question: { type: String, required: true },
   correctOption: { type: String, required: true },
-  difficulty: { type: Number, required: true }
+  difficulty: { type: Number, required: true },
+  approved: { type: Boolean },
+  createdBy: { type: String, ref: "User" },
+  approvedBy: { type: String, ref: "User" }
 });
 
 const KnowledgeQuestion = mongoose.model(
