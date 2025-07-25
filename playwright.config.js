d8eda5d61a1a2a9cd28d8146b8af0a995895c507
tohyzhong <tohyzhong@gmail.com>
2025-07-25 16:46:55 +0800

    feat: testing question base and playwright configh setup

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  timeout: 30_000,
+  timeout: 90_000,
   workers: 1,
   use: {
     baseURL: "http://localhost:5173",
