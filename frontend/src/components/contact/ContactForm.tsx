30daed5a3dc0b0f9328fac84a0fa3bc322817732
tohyzhong <tohyzhong@gmail.com>
2025-07-27 01:37:50 +0800

    test push

--- frontend/src/components/contact/ContactForm.tsx
+++ frontend/src/components/contact/ContactForm.tsx
@@ -29,7 +29,7 @@ const ContactForm = () => {
   };
 
   const isValidEmail = (email) => {
-    return true;
+    // return true;
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return emailRegex.test(email);
   };
