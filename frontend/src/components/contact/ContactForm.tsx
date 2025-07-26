bbc30bc23f81267620caaf5726ab73727920e710
tohyzhong <tohyzhong@gmail.com>
2025-07-27 01:53:03 +0800

    test push

--- frontend/src/components/contact/ContactForm.tsx
+++ frontend/src/components/contact/ContactForm.tsx
@@ -29,7 +29,6 @@ const ContactForm = () => {
   };
 
   const isValidEmail = (email) => {
-    return true;
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return emailRegex.test(email);
   };
